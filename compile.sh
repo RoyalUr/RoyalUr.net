4ef8cd2a72ab48200c8ef9a69520696c0bc91000
Padraig Lamont <padraiglamont@gmail.com>
2021-02-26 16:04:40 +0800

    #132: Add support for talking to the RoyalUrAnalysis project

--- compile.sh
+++ compile.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
+# Optionally run a script before compiling the site.
+if test -f "./pre_compile.sh"; then
+  ./pre_compile.sh
+fi
+
+
+# Compile the website!
 python3 -m compile "$@" || exit 1
 
+# Optionally run a script after compiling the site.
 if test -f "./post_compile.sh"; then
   ./post_compile.sh
 fi
---
+++ pre_compile.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp -f ../../Java/RoyalUrAnalysis/target/bytecoder/royal_ur_analysis.js res/royal_ur_analysis/bytecoder.js
+cp -f ../../Java/RoyalUrAnalysis/target/bytecoder/royal_ur_analysis.wasm res/royal_ur_analysis/program.wasm
+cp -f ../../Java/RoyalUrAnalysis/target/bytecoder/royal_ur_analysis.wasm.map res/royal_ur_analysis/program.wasm.map
+cp -f ../../Java/RoyalUrAnalysis/target/bytecoder/royal_ur_analysis.wat res/royal_ur_analysis/program.wat
