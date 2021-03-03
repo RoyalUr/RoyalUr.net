4c7cafb809d5f31fd78db27539dd91dac9d2d62c
Padraig Lamont <padraiglamont@gmail.com>
2021-03-03 10:52:17 +0800

    Remove pre_compile.sh custom to my own computer

--- pre_compile.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rsync -azP --delete-after ../../Java/RoyalUrAnalysis/target/bytecoder/ res/royal_ur_analysis/
-cp -f res/royal_ur_analysis/localedata.properties res/royal_ur_analysis/localedata_US.properties
-cp -f res/royal_ur_analysis/localedata.properties res/royal_ur_analysis/localedata_US_en.properties
